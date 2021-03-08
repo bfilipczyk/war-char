99153d38fcdf372b78dec0d5d9d4a8f0bce8fc4d
bfilipczyk <bfilipczyk119@gmail.com>
2021-03-08 18:25:25 +0100

    Starting commit.

---
+++ src/main/java/com/example/warchar/WarcharApplication.java
@@ -0,0 +1,13 @@
+package com.example.warchar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarcharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarcharApplication.class, args);
+	}
+
+}
